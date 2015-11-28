e45b8649124a713af15f3d527cc128db56a6e1ea
targhed <targhed@targhed-PC>
2015-11-28 13:03:17 +0100

    Refactor fxml, per hand

--- src/controller/Start.java
+++ src/controller/Start.java
@@ -7,7 +7,7 @@ package controller;
  * @date 20.11.2015 by Danilo: Initialisierung
  * @date 23.11.2015 by Danilo: Kommentare ergänzt
  */
-public class Start {
+//public class Start {
     
     /**
      * Einsprungpunkt der Software und starten aller benötigten Komponenten.
@@ -17,9 +17,12 @@ public class Start {
      * @date 20.11.2015 by Danilo: Initialisierung
      * @date 23.11.2015 by Danilo: Kommentar angepasst
      */
+    
+   /* 
     public static void main(String[] args) {
         SystemController.run();
         GuiController.run();
     }
     
 }
+*/
