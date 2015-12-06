9a2f51ed934eb4092471f1aabe399f3d33ae6331
Tobias <Tobias@Tobias-PC.fritz.box>
2015-12-06 15:55:37 +0100

    Filebrowser zeigt nur Verzeichnisse und .jpg Datein an.
    Problem für später: Hardlinks ausblenden

--- src/view/FilePathTreeItem.java
+++ src/view/FilePathTreeItem.java
@@ -5,10 +5,14 @@
  */
 package view;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -33,15 +37,25 @@ public class FilePathTreeItem extends TreeItem<String>{
     private boolean isDirectory;
     public boolean isDirectory(){return(this.isDirectory);}
     
-    public FilePathTreeItem(Path file){
+    private boolean isJPEG;
+    public boolean isJPEG(){return(this.isJPEG);}
+    
+    public FilePathTreeItem(Path file) throws IOException{
         super(file.toString());
         this.fullPath=file.toString();
+        
+        Path jpg = Paths.get(".jpg");
         //test if this is a directory and set the icon
-        if(Files.isDirectory(file)){
+        if(Files.isDirectory(file, LinkOption.NOFOLLOW_LINKS) && !Files.isHidden(file)){
             this.isDirectory=true;
+            this.isJPEG=false;
             //this.setGraphic(new ImageView(folderCollapseImage));
+        }else if(file.toString().endsWith(jpg.toString())){
+            this.isDirectory=false;
+            this.isJPEG=true;
         }else{
             this.isDirectory=false;
+            this.isJPEG=false;
             //this.setGraphic(new ImageView(fileImage));
             //if you want different icons for different file types this is where you'd do it
         }
@@ -67,77 +81,23 @@ public class FilePathTreeItem extends TreeItem<String>{
                         //iv.setImage(folderExpandImage);
                     }
                 try{
-                    if(source.getChildren().isEmpty()){
-                        Path path=Paths.get(source.getFullPath());
-                        BasicFileAttributes attribs=Files.readAttributes(path,BasicFileAttributes.class);
-                        if(attribs.isDirectory()){
-                            DirectoryStream<Path> dir=Files.newDirectoryStream(path);
-                            for(Path file:dir){
-                                FilePathTreeItem treeNode=new FilePathTreeItem(file);
+                    source.getChildren().remove(0, source.getChildren().size());
+                    Path path=Paths.get(source.getFullPath());
+                    BasicFileAttributes attribs=Files.readAttributes(path,BasicFileAttributes.class);
+                    if(attribs.isDirectory()){
+                        DirectoryStream<Path> dir=Files.newDirectoryStream(path);
+                        for(Path file:dir){
+                            FilePathTreeItem treeNode=new FilePathTreeItem(file);
+                            if(treeNode.isDirectory()){
+                                //Wenn Direcotory ausklappbar machen
+                                treeNode.getChildren().add(null);
+                                source.getChildren().add(treeNode);
+                            }
+                            //Wenn .jpg anzeigen
+                            else if(treeNode.isJPEG()){
                                 source.getChildren().add(treeNode);
                             }
                         }
-                    }else{
-                        //if you want to implement rescanning a directory for changes this would be the place to do it
-                    }
-                }catch(IOException x){
-                    x.printStackTrace();
-                }
-            }
-        });
-     
-        this.addEventHandler(TreeItem.branchCollapsedEvent(),new EventHandler(){
-            @Override
-            public void handle(Event e){
-                FilePathTreeItem source=(FilePathTreeItem)e.getSource();
-                if(source.isDirectory()&&!source.isExpanded()){
-                    ImageView iv=(ImageView)source.getGraphic();
-                    //iv.setImage(folderCollapseImage);
-                }
-            }
-        });
-    }
-    
-    
-    public FilePathTreeItem(String file){
-        super(file.toString());
-        this.fullPath=file.toString();
-        //test if this is a directory and set the icon
-        this.isDirectory=true;
-        
-        //set the value
-        if(!fullPath.endsWith(File.separator)){
-            //set the value (which is what is displayed in the tree)
-            String value=file.toString();
-            int indexOf=value.lastIndexOf(File.separator);
-                if(indexOf>0){
-                this.setValue(value.substring(indexOf+1));
-            }else{
-                this.setValue(value);
-            }
-        }
-    
-        this.addEventHandler(TreeItem.branchExpandedEvent(),new EventHandler(){
-            @Override
-            public void handle(Event e){
-                FilePathTreeItem source=(FilePathTreeItem)e.getSource();
-                    if(source.isDirectory()&&source.isExpanded()){
-                        ImageView iv=(ImageView)source.getGraphic();
-                        //iv.setImage(folderExpandImage);
-                    }
-                try{
-                    if(source.getChildren().isEmpty()){
-                        Path path=Paths.get(source.getFullPath());
-                        BasicFileAttributes attribs=Files.readAttributes(path,BasicFileAttributes.class);
-                        if(attribs.isDirectory()){
-                            DirectoryStream<Path> dir=Files.newDirectoryStream(path);
-                            for(Path file:dir){
-                                FilePathTreeItem treeNode=new FilePathTreeItem(file);
-                                source.getChildren().add(treeNode);
-                            }
-                        }
-                    }else{
-                        //if you want to implement rescanning a directory for changes this would be the place to do it
                     }
                 }catch(IOException x){
                     x.printStackTrace();
