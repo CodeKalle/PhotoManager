dea09c37f4bd2632fcf42222d905fa16a48fa8db
Tobias <Tobias@Tobias-PC.fritz.box>
2016-01-09 20:33:36 +0100

    Präsentation läuft

--- src/view/GuiPlayPraesentation.java
+++ src/view/GuiPlayPraesentation.java
@@ -1,16 +1,23 @@
 package view;
 
 import controller.FotoController;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * Diese Klasse dient als Startbildschirm
@@ -24,18 +31,34 @@ public class GuiPlayPraesentation implements Initializable{
 
     @FXML
     private ImageView guiPlayPraesentationImageView;
+    @FXML
+    private Button guiPlayPraesentationBack;
 
+    Thread praesi;
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Titel setzten
         Main.getPrimaryStage().setTitle("Photomanager - PlayPraesentation.fxml");
         
-        Thread praesi = new Praesentation();
+        praesi = new Praesentation();
         praesi.start();
     }
 
-    
+    @FXML
+    public void handleButtonAction(ActionEvent event) throws IOException, InterruptedException{
+        Stage stage;
+        Parent root;        
+        
+        
+        praesi.interrupt();
+        stage=(Stage) guiPlayPraesentationBack.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("GuiSelectPraesentation.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     /**
      * Thread Klasse für die Präsentation
      * 
@@ -43,6 +66,7 @@ public class GuiPlayPraesentation implements Initializable{
      * @date 09.01.2016 by Tobias: Initialisierung
      */
     class Praesentation extends Thread{
+        
         public void run() {
             List<Path> fotos = FotoController.getFotosFromAlbum(Main.speicher);
         
@@ -50,7 +74,10 @@ public class GuiPlayPraesentation implements Initializable{
                 try {
                     Image image = new Image(foto.toUri().toString());
                     guiPlayPraesentationImageView.setImage(image);
-                    sleep(5000);
+                    
+                    for(int i = 0; i < 50; i++){
+                        sleep(100);
+                    }
                 } catch (InterruptedException ex) {
                     Logger.getLogger(GuiPlayPraesentation.class.getName()).log(Level.SEVERE, null, ex);
                 }
