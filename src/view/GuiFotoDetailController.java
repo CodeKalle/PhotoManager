a5450dc6c55495120eaadc6bece2d214a2e41493
targhed <targhed@targhed-PC>
2016-01-13 15:31:41 +0100

    GuiAddFoto Positionierung GuiFotoDetail hinzugefügt

--- src/view/GuiFotoDetailController.java
+++ src/view/GuiFotoDetailController.java
@@ -1,15 +1,27 @@
 package view;
 
+import controller.FotoController;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 /**
@@ -21,6 +33,7 @@ import javafx.stage.Stage;
  * @date ??.??.2015 by Juliane & Manuel: Konzept der GUIs und programmtechnische Umsetzung
  * @date 06.12.2015 by Tobias: Initialize erweitert
  * @date 10.12.2015 by Danilo: Kommentare ergänzt
+ * @date 13.01.2016 by Manuel: rework
  */
 public class GuiFotoDetailController implements Initializable{
     
@@ -32,8 +45,13 @@ public class GuiFotoDetailController implements Initializable{
     * @date 10.12.2015 by Danilo: Kommentare ergänzt
     */
     // Buttons der Klasse
-    @FXML
-    Button guiFotoDetailAbbrechen, guiFotoDetailBearbeiten;
+    
+    @FXML ScrollPane guiFotoDetailScrollPane;
+    @FXML TilePane guiFotoDetailTilePane;
+    @FXML Button guiFotoDetailAbbrechen, guiFotoDetailBearbeiten;
+    @FXML ImageView guiFotoDetailImageView;
+    @FXML TextFlow guiAddFotoDetailTextFlow;
+    @FXML TextArea guiAddFotoDetailTextArea;
     
     /**
     * Methode handelt die Aktionen der Buttons
@@ -45,6 +63,7 @@ public class GuiFotoDetailController implements Initializable{
     * @date ??.11.2015 by Tobias: Initialisierung
     * @date 10.12.2015 by Danilo: Kommentare ergänzt
     */
+   
     @FXML
     public void handleButtonAction(ActionEvent event) throws IOException{
         Stage stage;
@@ -76,6 +95,52 @@ public class GuiFotoDetailController implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Main.getPrimaryStage().setTitle("PhotoManager - Details");
+        
+    for(int i = 0; i < FotoController.getFotosFromAlbum(Main.speicher).size(); i++) {
+            //Für jedes Bild Konstrukt zusammensetzen
+            Pane lpane = new Pane();            
+            lpane.setPrefSize(80, 100);
+            
+            Image image = new Image(FotoController.getFotosFromAlbum(Main.speicher).get(i).toUri().toString());
+            
+            ImageView imageView = new ImageView();
+            imageView.setFitHeight(80);
+            imageView.setFitWidth(80);
+            imageView.setPickOnBounds(true);
+            imageView.setPreserveRatio(true);
+            imageView.setImage(image);  
+            
+            imageView.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+
+                    guiFotoDetailImageView.setImage(image);
+                    guiAddFotoDetailTextArea.setVisible(true);
+                    guiAddFotoDetailTextArea.setText("test 1234!");
+                    
+
+                }
+            });
+            
+            
+            Label name = new Label();
+            name.setLayoutX(20.0);
+            name.setLayoutY(80.0);
+            name.setPrefHeight(20);
+            name.setPrefWidth(80);
+            name.setText(FotoController.getFotosFromAlbum(Main.speicher).get(i).getFileName().toString());
+            
+            Label pfad = new Label();
+            pfad.setVisible(false);
+            pfad.setText(FotoController.getFotosFromAlbum(Main.speicher).get(i).toString());
+
+            lpane.getChildren().add(imageView); //ID 0
+            lpane.getChildren().add(name);      //ID 1
+            lpane.getChildren().add(pfad);      //ID 2
+            
+            //Fertiges Konstrukt in Pane anzeigen
+            guiFotoDetailTilePane.getChildren().add(i, lpane);
+        }
     }
     
 }
